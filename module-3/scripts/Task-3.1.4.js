c7a3990e523bc10d035e0d90368dfd6bab9f0bec
sktrip <sktripurari@gmail.com>
2020-11-24 11:28:20 +0000

    Update Task-3.1.4.js

--- module-3/scripts/Task-3.1.4.js
+++ module-3/scripts/Task-3.1.4.js
@@ -1,10 +1,10 @@
 var intValue1 = 20 ;
 var intValue2 = 3 ;
 
-var val3 = intValue1*intValue2
+var multValue = intValue1*intValue2
 var addValue = intValue1+intValue2
 var subValue = intValue1-intValue2
-alert(val3);
+alert(multValue);
 
 console.log(addValue);
 
