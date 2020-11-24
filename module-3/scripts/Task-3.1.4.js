fdb8089abe63fd667a761d780f508acd85e5a287
sktrip <sktripurari@gmail.com>
2020-11-24 11:17:02 +0000

    Task 3.1.4
    Variable with arithmetic operations

---
+++ module-3/scripts/Task-3.1.4.js
@@ -0,0 +1,11 @@
+var intValue1 = 20 ;
+var intValue2 = 3 ;
+
+var val3 = intValue1*intValue2
+var addValue = intValue1+intValue2
+var subValue = intValue1-intValue2
+alert(val3);
+
+console.log(addValue);
+
+document.write(subValue);
