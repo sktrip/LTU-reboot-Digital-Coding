57b6a83a56c12b9b7d61004d929346cd90efe8e3
sktrip <sktripurari@gmail.com>
2020-11-25 09:50:22 +0000

    Create Functions.js

---
+++ module-3/scripts/Functions.js
@@ -0,0 +1,10 @@
+/*
+Declaring a function
+
+Decaring a function using functions keyword, followed by the function namme, list of parameters and the function body
+
+*/
+
+function greetings(name) {
+    console.log(name);    
+}
