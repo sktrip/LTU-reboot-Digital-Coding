37d5678914cfc57109b8fd0b5b99090c803b46fc
sktrip <sktripurari@gmail.com>
2020-11-26 22:59:21 +0000

    Task 3.5.2

---
+++ module-3/scripts/Task-3.5.2.js
@@ -0,0 +1,78 @@
+let shoppingCart = [
+    {
+      name:"loaf of bread",
+      type:"food",
+      quantity:1,
+      price:.85
+    },
+    { 
+      name:"multipack beans",
+      type:"food",
+      quantity:1,
+      price:1
+  },
+  { 
+    name:"mushrooms",
+    type:"food",
+    quantity:10,
+    price:.1
+  },
+  {
+    name:"can of beer",
+    type:"alcohol",
+    quantity:4,
+    price:1.1
+  },
+  {
+    name:"prosecco",
+    type:"alcohol",
+    quantity:1,
+    price:8.99
+  },
+  {
+    name:"steak",
+    type:"food",
+    quantity:2,
+    price:3.99
+  },
+  {
+    name:"blue cheese",
+    type:"food",
+    quantity:1,
+    price:2.99
+  },
+  {
+    name:"candles",
+    type:"home",
+    quantity:3,
+    price:1.99
+  },
+  { 
+    name:"cheesecake",
+    type:"food",
+    quantity:1,
+    price:4.99
+  },
+  { 
+    name:"onions",
+    type:"food",
+    quantity:3,
+    price:.4
+  }];
+
+  function shoppingCartTotalPrice( arr ) {
+    let totalPrice = 0;
+    for(i=0; i<arr.length; i++) {
+        if(arr[i].type=='food') {
+
+        totalPrice += ((arr[i].quantity * arr[i].price)*0.8);
+        }
+      else {
+        totalPrice += (arr[i].quantity * arr[i].price);
+      }
+         
+    }
+    return totalPrice.toFixed(2);
+}
+    var totalCost = shoppingCartTotalPrice(shoppingCart);
+    console.log('Total cost of the shopping cart items is : '+totalCost);
