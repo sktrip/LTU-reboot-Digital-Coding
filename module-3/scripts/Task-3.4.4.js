8219f112d3169d3f98895ae60c6a109de2713227
sktrip <sktripurari@gmail.com>
2020-11-26 15:57:41 +0000

    Task 3.4.4.js

---
+++ module-3/scripts/Task-3.4.4.js
@@ -0,0 +1,13 @@
+var recipe = {
+    recipeTitle : 'Rice kheer',
+    ingredients : ['rice', 'milk', 'sugar' ],
+    servings : 4,
+    directions : 'Boil the milk and add rice and sugar'
+}
+
+console.log(recipe.directions)
+console.log(recipe.length)
+
+for( i=0; i<recipe.length ; i++) {
+    console.log(recipe.directions)
+}
