502983197564a1c82af3f0c29aff09ae3469e9d1
sktrip <sktripurari@gmail.com>
2020-11-27 10:55:07 +0000

    Task-3.6.1

---
+++ module-3/scripts/Task-3.6.1.js
@@ -0,0 +1,8 @@
+let image = document.getElementById('imageId');
+
+image.src = 'http://2.bp.blogspot.com/-GFWjZsVR3ps/T3lIYA1uB7I/AAAAAAAABqo/BTr0HAeW7fQ/s1600/Google+Wallpapers+4.jpg'
+
+var paragraph = document.getElementById('paragraph');
+
+paragraph.style.color = 'green'
+
