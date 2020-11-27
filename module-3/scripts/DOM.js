9f8f99f04693914212030dc94c9bcf224841f4b9
sktrip <sktripurari@gmail.com>
2020-11-27 10:19:51 +0000

    DOM

---
+++ module-3/scripts/DOM.js
@@ -0,0 +1,13 @@
+let heading1 = document.querySelector('h1');
+console.log(heading1);
+
+let paragraph = document.querySelector('p');
+console.log(paragraph);
+
+let listItems = document.querySelectorAll('li')
+
+console.log(listItems[1]);
+
+let item = document.getElementsByClassName('item');
+
+console.log(item);
